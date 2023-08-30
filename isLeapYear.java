517e6f81533cff632a50d8933aa4dc10407a9c90
ShahabUddin <s4shahab@gmail.com>
2023-08-30 13:15:15 +0200

    isLeapYear

---
+++ isLeapYear.java
@@ -0,0 +1,17 @@
+public class isLeapYear {
+    public static boolean isLeapYear(int year){
+        if (year<1 || year>9999){
+            return false;
+        }
+        else if((year%4==0 && year%100!=0) &&(year>=1 && year<=9999) || year%400==0 &&(year>=1&&year<=9999)) {
+            return true;
+        }
+
+        else{
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+}
