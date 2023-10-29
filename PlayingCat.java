a74f5838bc0632925850044a7cc95631453e367e
ShahabUddin <s4shahab@gmail.com>
2023-10-29 16:17:31 +0100

    PlayingCat

---
+++ PlayingCat.java
@@ -0,0 +1,21 @@
+public class PlayingCat {
+        public static boolean isCatPlaying(boolean summer, int temperature){
+            if (temperature>=25 && temperature <=35 && summer==false){
+                return true;
+            }
+
+        else if (temperature>=25 && temperature <=45 && summer==true){
+                return true;
+            }
+
+        else {
+                return false;
+            }
+        }
+
+    public static void main(String[] args) {
+
+    }
+
+    }
+
