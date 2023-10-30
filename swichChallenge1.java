fe21b7e831a9df95b9bfb4b952eff8eed9dcd7eb
ShahabUddin <s4shahab@gmail.com>
2023-10-30 01:46:51 +0100

    switchChallenge

---
+++ swichChallenge1.java
@@ -0,0 +1,27 @@
+public class swichChallenge1 {
+    public static void main(String[] args) {
+        char value='C';
+        switch (value){
+            case 'A':
+                System.out.println("Able");
+                break;
+            case 'B':
+                System.out.println("Baker");
+                break;
+            case 'C':
+                System.out.println("Charlie");
+                break;
+            case 'D':
+                System.out.println("Dog");
+                break;
+            case 'E':
+                System.out.println("Easy");
+                break;
+            default:
+                System.out.println("not found");
+                break;
+
+
+        }
+    }
+}
